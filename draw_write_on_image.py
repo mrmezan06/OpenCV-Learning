18edd85c7d958f978cc7715dbf982f70ced0caed
mezan <mrmezan0@gmail.com>
2020-01-16 23:23:23 +0600

    Draw on Image

---
+++ draw_write_on_image.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plot
+
+img = cv2.imread('akash.jpg', cv2.IMREAD_COLOR)
+# Color System in CV2 BGR
+# cv2.line(img, (0, 0), (200, 200), (0, 255, 0), 15)
+# rectangle(image, startpt1(x,y), Drawpt2(x,y), BGR(255,255,255),linewidth)
+cv2.rectangle(img, (100, 200), (450, 600), (0, 255, 0), 10)
+cv2.imshow('Line Draw', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
