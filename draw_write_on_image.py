590659d770ad759d88791f5f15250bfc9a46fea0
mezan <mrmezan0@gmail.com>
2020-01-17 00:34:05 +0600

    Polyline on Image

--- draw_write_on_image.py
+++ draw_write_on_image.py
@@ -8,6 +8,10 @@ img = cv2.imread('akash.jpg', cv2.IMREAD_COLOR)
 # rectangle(image, startpt1(x,y), Drawpt2(x,y), BGR(255,255,255),linewidth)
 cv2.rectangle(img, (100, 200), (450, 600), (0, 255, 0), 10)
 cv2.circle(img, (450, 200), 55, (0, 0, 255), -1)
+
+pts = np.array([[10, 5], [20, 30], [70, 20], [50, 10]], np.int32)
+cv2.polylines(img, [pts], True, (0, 255, 0), 3)
+
 cv2.imshow('Line Draw', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
